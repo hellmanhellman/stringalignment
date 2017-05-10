7f92b137bc4685ccfacde9d89d1b565a9441f862
Jonas Hellman <jonashellman@gmail.com>
2017-05-10 10:26:04 +0200

    commentary

--- stringalignment.hs
+++ stringalignment.hs
@@ -3,7 +3,7 @@
 -- A:  Set scoreMatch = 1 and the others to 0.
 
 -- Q2: Explain what the following Haskell function does.
--- A: It takes a list of tuples of lists and attaches a prefix to each tuple-list.
+-- A: It takes a list of tuples of lists and attaches a two prefixes to each tuple in the list.
 --    eg. attachHeads "un" "super" [("cool","sweet")] -> [("uncool","supersweet")]
 
 
@@ -19,17 +19,24 @@ string2 = "vintner"
 
 type AlignmentType = (String,String)
 
+
 max3 a b c = max a (max b c)
 
+-- Calculates the similarity score between two strings.
+-- Implementation is self-documenting.
 similarityScore :: String -> String -> Int
 similarityScore [] xs = scoreSpace * (length xs)
 similarityScore xs [] = scoreSpace * (length xs)
 
-
+-- Could've used maximum and a list here, but looks better with max3.
 similarityScore (x:xs) (y:ys) = max3 ((similarityScore xs ys) + (score x y))
                                      ((similarityScore xs (y:ys)) + (score x '-'))
                                      ((similarityScore (x:xs) ys) + (score '-' y))
 
+
+-- More efficient implementation, using memoization.
+-- This means that no (or at least much fewer) redundant calculations, as
+-- reusable data is stored in a lookup table during the computation.
 similarityScore' :: String -> String -> Int
 similarityScore' xs ys = simScore (length xs) (length ys)
   where
@@ -47,6 +54,7 @@ similarityScore' xs ys = simScore (length xs) (length ys)
          x = xs!!(i-1)
          y = ys!!(j-1)
 
+-- Returns the score of two compared characters.
 score :: Char -> Char -> Int
 score _ '-' = scoreSpace
 score '-' _  = scoreSpace
@@ -54,14 +62,9 @@ score x y = if x == y
             then scoreMatch
             else scoreMismatch
 
+
 maximaBy :: Ord b => (a -> b) -> [a] -> [a]
 maximaBy valueFcn xs = [a | a <- xs, valueFcn a == valueFcn (maximumBy (comparing valueFcn) xs)]
-
-
-
-
-
-main = print $ similarityScore' string1 string2
 
 
 attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])]
@@ -70,48 +73,51 @@ attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
 attachTails :: a -> a -> [([a],[a])] -> [([a],[a])]
 attachTails h1 h2 aList = [(xs++[h1],ys++[h2]) | (xs,ys) <- aList]
 
+-- Outputs a list of all the optimum alignments. Memoized.
 optAlignments :: String -> String -> [AlignmentType]
 optAlignments xs ys = snd (optAlign (length xs) (length ys))
         where
           optAlign :: Int -> Int -> (Int, [AlignmentType])
           optAlign i j = table!!i!!j
           table = [[optEntry i j | j <- [0..]] | i <- [0..]]
-          
+
           optEntry :: Int -> Int -> (Int, [AlignmentType])
           optEntry 0 j = (scoreSpace*j, [(replicate j '-',take j  ys)])
           optEntry i 0 = (scoreSpace*i, [(take i xs, replicate i '-')])
           optEntry i j  = (simScore, [alignment | alignment <- alignments, alignmentScore alignment == simScore])
 --          optEntry i j  = (simScore, maximaBy alignmentScore alignments) -- this is slow
             where
-              
+
               ls1 | score == bestAdjacentScore = attachTails x y (snd (optAlign (i-1) (j-1)))
                   | otherwise                  = []
                  where score = fst (optAlign (i-1) (j-1))
-                
+
               ls2 | score == bestAdjacentScore = attachTails '-'  y  (snd (optAlign i (j-1)))
                   | otherwise                  = []
                  where score = fst (optAlign i (j-1))
-                 
+
               ls3 | score == bestAdjacentScore = attachTails x  '-'  (snd (optAlign (i-1) j))
                   | otherwise                  = []
                  where score = fst (optAlign (i-1) j)
-                 
+
               bestAdjacentScore = max3 (fst (optAlign (i-1) (j-1))) (fst (optAlign i (j-1))) (fst (optAlign (i-1) j))
               alignments =  ls1 ++ ls2 ++ ls3
               simScore = similarityScore' (take i xs) (take j ys)
               x = xs!!(i-1)
               y = ys!!(j-1)
 
-alignmentScore :: (String, String) -> Int
+-- Calculates the score of an alignment.
+-- Almost the same as similarityScore, but this function calculates the score of an alignment,
+-- whereas similarityScore calculates the best possible score of two strings.
+alignmentScore :: AlignmentType -> Int
 alignmentScore ([], xs) = scoreSpace * (length xs)
 alignmentScore (xs, []) = scoreSpace * (length xs)
 alignmentScore ((x:xs), (y:ys)) = alignmentScore (xs,ys) + score x y
 
-spacey :: String -> String
-spacey (x:[]) = [x]
-spacey (x:xs) = [x,' '] ++ spacey xs
 
 
+
+-- Pretty prints the alignments.
 outputOptAlignments :: String -> String -> IO ()
 outputOptAlignments xs ys = do  putStrLn $ "There are " ++ show (length result) ++ " optimal alignments:"
                                 putStrLn ""
@@ -125,4 +131,9 @@ outputOptAlignments xs ys = do  putStrLn $ "There are " ++ show (length result) 
                                                               putStrLn ""
                                                               if xs /= [] then printAlignments xs
                                                                 else return ()
+                                  spacey (x:[]) = [x]
+                                  spacey (x:xs) = [x,' '] ++ spacey xs
 
+long1 = "aferociousmonadatemyhamster"
+long2 = "functionalprogrammingrules"
+main = outputOptAlignments long1 long2
